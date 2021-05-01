91252420acbc7abe4e68e053d9653f1f398e3c25
amer0077 <78500870+amer0077@users.noreply.github.com>
2021-05-02 02:00:59 +0300

    Add files via upload

---
+++ hooks-master/src/Comp/MovieData.js
@@ -0,0 +1,8 @@
+export const MovieData=[
+    { id:0, img:"https://imgsrc.cineserie.com/2019/02/1564824.jpg?ver=1" ,
+    name:"I SEE YOU" ,type:"Action" , rate:1},
+    { id:1,img:"https://fr.web.img3.acsta.net/pictures/17/01/25/09/36/363964.jpg" ,
+    name:"LION" ,type:"..." ,rate:3},
+    
+]
+
